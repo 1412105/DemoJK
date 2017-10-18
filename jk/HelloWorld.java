5ca55ac9fa621fb8aa17bd60929f681800c1565a
Unknown <1412105@student.hcmus.edu.vn>
2017-10-18 12:09:16 +0700

    Test #1

---
+++ jk/HelloWorld.java
@@ -0,0 +1,13 @@
+package jk;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		// TestClass t = new TestClass();
+		// System.out.println(t.value.charAt(0));
+	}
+}
+
+// class TestClass {
+// 	String value;
+// }
