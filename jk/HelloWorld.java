72c5cc6ea9bbfb91a7cf6e911eb6efb35c0db1a2
Unknown <1412105@student.hcmus.edu.vn>
2017-10-19 21:37:07 +0700

    Test #4

--- jk/HelloWorld.java
+++ jk/HelloWorld.java
@@ -3,6 +3,7 @@ package jk;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("Welcome to Jenkins");
 		// TestClass t = new TestClass();
 		// System.out.println(t.value.charAt(0));
 	}
